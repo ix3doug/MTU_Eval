289f8653356d6fa4e286bea8f007194c6a5d2ba7
dougspence <dougietc3@proton.me>
2026-08-01 13:16:15 -0400

    Next task completed I think

--- oddSum.cpp
+++ oddSum.cpp
@@ -1,3 +1,14 @@
-//
-// Created by Doug Spence on 7/31/26.
-//
+#include <iostream>
+
+int main() {
+
+    int count = 0;
+
+    for (int i = 17; i <= 329; i = i + 2) {
+
+        count = count + i;
+
+    }
+
+    std::cout << "the sum is " << count;
+}
