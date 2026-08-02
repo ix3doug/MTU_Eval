83e25019e06d09895ce8f9b2b94e86846aff46c9
ix3doug <dspence@wwbay.com>
2026-08-02 16:13:53 -0400

    Next task created and completed

---
+++ printNumPair.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+    for (int i = 1; i <= 9; i++) {
+
+        for (int n = 1; n <= i; n++) {
+
+            std::cout << i;
+
+            std::cout << n << '\n';
+
+        }
+
+    }
+
+}
